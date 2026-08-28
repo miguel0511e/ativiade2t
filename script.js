7b4705faedeb5b0785349e239d57e7b6768739f5
miguel0511e <miguel.mariano.machado@escola.pr.gov.br>
2026-08-28 12:38:12 -0300

    p

--- script.js
+++
@@ -1,8 +0,0 @@
-const botao = document.querySelector{"button"};
-    botao.addEventListener("click", botaoClicado);
-
-    function botaoClicado(){
-        console.log("fui clicado");
-        let texto = botao.querySelector("span");
-        texto.textContent++;
-}
